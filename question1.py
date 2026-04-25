ea9a4d740984bcef3b6676adcc17795ecaef7366
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-04-25 12:59:44 -0500

    PROBLEMA 1

--- question1.py
+++ question1.py
@@ -10,13 +10,15 @@ try:
     total_load=int(sys.argv[1])
     num_supports=int(sys.argv[2])
     load_per_support = total_load / num_supports
+    if num_supports==0:
+        print("Error: Cannot divide by zero! Supports must be greater than zero.")
     print(f"Load per support point: {load_per_support:.2f} N")
-        
+
 except ValueError: 
     print("Error: Invalid input! Enter numeric values only.")
-
-except num_supports==0:
-    print("Error: Cannot divide by zero! Supports must be greater than zero.")
-
-except total_load==None or num_supports==None:
+except IndexError:
     print("Error: Invalid input! Enter numeric values only.")
+
+
+
+
