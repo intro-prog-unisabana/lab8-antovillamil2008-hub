f79dd49b0624ca591038a4913830028642c4d36a
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-04-25 17:55:11 -0500

    2

--- main.py
+++ main.py
@@ -5,8 +5,21 @@ import sys
 from todo_manager import read_todo_file, write_todo_file
 
 try: 
-    nombre=(sys.argv[1])
-    ruta=(sys.argv[2])
-    print("")
-except IndexError:
-    print("Insufficient arguments provided!")
+    if len(sys.argv)<0:
+        raise IndexError("Insufficient arguments provided!")
+    
+    ruta=sys.argv[1]
+    tareas=read_todo_file(ruta)
+    if len(sys.argv)>2: 
+        c=sys.argv[2]
+
+
+print("Command-line arguments:")
+for arg in sys.argv[1:]:
+    print(arg)
+print("Tasks")
+for t in tareas:
+    print(t)
+
+
+    
