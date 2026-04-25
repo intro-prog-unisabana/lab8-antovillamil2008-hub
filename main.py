74966071856dd2a9cbb700ce19af0e030b4c9202
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-04-25 18:13:46 -0500

    ayudaaaaaaa

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ try:
     
     ruta=sys.argv[1]
     tareas=read_todo_file(ruta)
+    if ruta=="--help":
+        print("""Usage: python main.py <file_path> <command> [arguments]...
+Commands:
+  add "task"    - Add a task to the list.
+  remove "task" - Remove a task from the list.
+  view          - Display all tasks.""")
+        
+        print("Command-line arguments:")
+    for arg in sys.argv[1:]:
+        print(arg)
+        print("\nTasks:")
+        for t in tareas:
+            print(t)
+    
+    tareas=
     if len(sys.argv)>2: 
         c=sys.argv[2]
         if c=="view":
