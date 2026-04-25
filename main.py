653c695815812d858cb3ca9c3034449cde320bdc
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-04-25 17:31:01 -0500

    1

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 """Laboratorio 8 - CLI del gestor de tareas."""
 
 # TODO: Implementar CLI según README.md
+import sys
+from todo_manager import read_todo_file, write_todo_file
+
+try: 
+    nombre=(sys.argv[1])
+    ruta=(sys.argv[2])
+    print("")
+except IndexError:
+    print("Insufficient arguments provided!")
