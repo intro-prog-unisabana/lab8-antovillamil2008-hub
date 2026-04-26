993dc9d6b27afe20bc543950698c5b003b5ecc2a
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-04-26 12:13:54 -0500

    no funcionaaaaaaaa

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ Examples:
                 print("Tasks:")
                 for t in tasks:
                     print(t)
-                    
                 i += 1  
 
             elif comando == "add":
